fe9b01a47e05f97e1836c4069b79e17851daf698
girindrafajarnugraha <indra.paradinata@gmail.com>
2023-03-16 22:50:22 +0700

    update

---
+++ database/migrations/2023_03_04_012509_create_pengembalians_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePengembaliansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pengembalians', function (Blueprint $table) {
+            $table->increments('id');
+            // $table->string('id_kembali', 10);
+            $table->string('tanggal_pinjam')->nullable();
+            $table->string('tanggal_kembali')->nullable();
+            $table->text('keterangan');
+            $table->integer('denda')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pengembalians');
+    }
+}
---
+++ resources/views/Petugas/edit.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-10">
+            <div class="card">
+                <div class="card-header">
+                    Edit Data Petugas
+                </div>
+                @if (Count($errors) > 0)
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $pesan)
+                        <li>
+                            {{$pesan}}
+                        </li>
+                        @endforeach
+                    </ul>
+                </div>  
+                @endif
+                <div class="card-body">
+                    {!! Form::model($data, ['route'=> ['pegawai.update', $data->id], 'method'=>'PUT']) !!}
+                        <div class="form-group">
+                            {!! Form::text('kode_petugas', null, ['class'=>'form-control', 'id'=> 'kode_petugas']) !!}
+                        </div>
+                        <div class="form-group">
+                            {!! Form::text('nama_petugas', null, ['class'=>'form-control', 'id'=> 'nama_petugas']) !!}
+                        </div>
+                        <div class="form-group">
+                            {!! Form::text('no_telpon_petugas', null, ['class'=>'form-control', 'id'=> 'no_telpon_petugas']) !!}
+                        </div>
+                        <div class="form-group">
+                            {!! Form::text('email_petugas', null, ['class'=>'form-control', 'id'=> 'email_petugas']) !!}
+                        </div>
+                        <div class="form-group">
+                            {!! Form::submit('Simpan', null, ['class'=>'btn btn-primary']) !!}
+                        </div>
+                    {!! Form::close() !!}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
